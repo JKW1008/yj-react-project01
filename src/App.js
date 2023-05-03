4fb3cf1a0f441039125cdd5ffcb40cf98b80a752
JungKangWoo <todendvv91@gmail.com>
2023-05-03 13:25:38 +0900

    sibal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { GlobalStyles } from "./styles";
+
+  const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: red;
+    display: flex;
+    justify-content: center;`
+  
+  const GlobalNav = styled.div`
+    width: 1000px;
+    background: blue;
+    color: white;
+    justify-content: center;`
+
+  const smallNav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    div{
+      padiing: 0 15px;
+    }
+    `
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyles/>
+      <Container>
+        <GlobalNav>
+          <div>logo</div>
+          <smallNav>
+          <div>업체소개</div>
+          <div>포트폴리오</div>
+          <div>사진</div>
+          <div>게시판</div>
+          <div>연락</div>
+          </smallNav>
+        </GlobalNav>
+      </Container>
     </div>
   );
 }
